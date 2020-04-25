ba73ab0b5fa5f7a6e570305f55ba726aae6c94a8
Cent <511737600@qq.com>
2020-04-25 17:27:52 +0800

    数据源配置完善

--- demo-datasource/src/main/java/com/cent/demo/datasource/config/DataSourceConfig.java
+++ demo-datasource/src/main/java/com/cent/demo/datasource/config/DataSourceConfig.java
@@ -1,8 +1,13 @@
 package com.cent.demo.datasource.config;
 
+import com.alibaba.fastjson.JSON;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -13,15 +18,43 @@ import javax.sql.DataSource;
  * @create: 2020/3/31
  **/
 @Configuration
+@Slf4j
 public class DataSourceConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Value("${mydatasource}")
+    private String myDataSourceJsonString;
+
+    @Autowired
+    private JdbcConfig jdbcConfig;
 
     @Bean("myDataSource")
     DataSource dataSource() {
+        // 获取环境变量配置方法一：先获取环境变量，再手工获取配置项
+        log.debug(environment.getProperty("mydatasource"));
+        // 获取环境变量配置方法二：自动配置
+        log.debug("mydatasource: {}", myDataSourceJsonString);
+
+        // jdbc配置属性获取方法一：每项属性在环境变量中独立单独配置
+        log.debug("jdbcConfig: {}", jdbcConfig);
+
+        // jdbc配置属性获取方法二：每项属性组成jsonString格式一起配置，获取配置jsonString再解析使用
+        JdbcConfig myJdbcConfig = JSON.parseObject(myDataSourceJsonString, JdbcConfig.class);
+        log.debug("myJdbcConfig: {}", myJdbcConfig);
+
         HikariDataSource ds = new HikariDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setJdbcUrl("jdbc:mysql://localhost:3306/mytest?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC");
-        ds.setUsername("root");
-        ds.setPassword("123456");
+//        ds.setDriverClassName(jdbcConfig.getDriver());
+//        ds.setJdbcUrl(jdbcConfig.getUrl());
+//        ds.setUsername(jdbcConfig.getUsername());
+//        ds.setPassword(jdbcConfig.getPassword());
+
+        ds.setDriverClassName(myJdbcConfig.getDriver());
+        ds.setJdbcUrl(myJdbcConfig.getUrl());
+        ds.setUsername(myJdbcConfig.getUsername());
+        ds.setPassword(myJdbcConfig.getPassword());
+
         return ds;
     }
 }
---
+++ demo-datasource/src/main/java/com/cent/demo/datasource/config/JdbcConfig.java
@@ -0,0 +1,28 @@
+package com.cent.demo.datasource.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @class: JdbcConfig
+ * @description: 自定义数据源配置
+ * @author: cent
+ * @create: 2020/4/25
+ **/
+@Configuration
+@Data
+public class JdbcConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+}
